0950ad29fc88b8c54adda8dc90e59669984bd2b8
Henrique Repulho <henrique.repulho@gmail.com>
2019-03-10 22:13:18 -0300

    commit

--- function_tst.go
+++ function_tst.go
@@ -2,10 +2,18 @@ package main
 
 import "fmt"
 
+//Uso de Constantes
+func constante() string {
+	const World = "abc"
+	return World
+}
+
+//Calculos Simples
 func soma(x, y int) int {
 	return x + y
 }
 
 func main() {
 	fmt.Println(soma(10, 30))
+	fmt.Println(constante())
 }
